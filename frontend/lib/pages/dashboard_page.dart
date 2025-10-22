1677d01329f3b421acb4c75bdcadeb474157047d
JENNIFER OJI <G00413791@atu.ie>
2025-10-22 21:17:24 +0100

    Create search bar on dashboard page (KAN-17)

--- frontend/lib/pages/dashboard_page.dart
+++ frontend/lib/pages/dashboard_page.dart
@@ -13,7 +13,26 @@ class _DashboardPageState extends State<DashboardPage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // search bar
+        Container(
+          padding: const EdgeInsets.all(10.0),
+          margin: const EdgeInsets.symmetric(horizontal: 25.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Search"),
+              Icon(Icons.search),
+            ],
+          ),
+        ) 
+
+        // message 
         
+        // stocks to choose from 
       ],
     );
   }
