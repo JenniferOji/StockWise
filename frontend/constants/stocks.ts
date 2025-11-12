8713dd90fa0d91e1c92ae9327426b63ab270c0d3
JENNIFER OJI <G00413791@atu.ie>
2025-11-12 12:25:12 +0000

    Update image urls to png to render on mobile

--- frontend/constants/stocks.ts
+++ frontend/constants/stocks.ts
@@ -5,89 +5,90 @@ export interface Stock {
   imageUrl: string;
 }
 
+
 export const STOCKS: Stock[] = [
   {
     symbol: 'AAPL',
     companyName: 'Apple Inc.',
     shares: 48,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/500px-Apple_logo_black.svg.png'
   },
   {
     symbol: 'META',
     companyName: 'Meta Platforms, Inc.',
     shares: 20,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Meta_Platforms_Logo_2023.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Meta_Platforms_Logo_2023.svg/500px-Meta_Platforms_Logo_2023.svg.png'
   },
   {
     symbol: 'NVDA',
     companyName: 'NVIDIA Corporation',
     shares: 10,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Nvidia_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Nvidia_logo.svg/500px-Nvidia_logo.svg.png'
   },
   {
     symbol: 'NFLX',
     companyName: 'Netflix, Inc.',
     shares: 8,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/500px-Netflix_2015_logo.svg.png'
   },
   {
     symbol: 'JPM',
     companyName: 'JPMorgan Chase & Co.',
     shares: 25,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/52/JPMorgan_Chase_Logo_2016.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/JPMorgan_Chase_Logo_2016.svg/500px-JPMorgan_Chase_Logo_2016.svg.png'
   },
   {
     symbol: 'BAC',
     companyName: 'Bank of America Corporation',
     shares: 30,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Bank_of_America_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Bank_of_America_logo.svg/500px-Bank_of_America_logo.svg.png'
   },
   {
     symbol: 'DIS',
     companyName: 'The Walt Disney Company',
     shares: 14,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Disney_wordmark.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Disney_wordmark.svg/500px-Disney_wordmark.svg.png'
   },
   {
     symbol: 'KO',
     companyName: 'The Coca-Cola Company',
     shares: 40,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Coca-Cola_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Coca-Cola_logo.svg/500px-Coca-Cola_logo.svg.png'
   },
   {
     symbol: 'PFE',
     companyName: 'Pfizer Inc.',
     shares: 18,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Pfizer_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Pfizer_logo.svg/500px-Pfizer_logo.svg.png'
   },
   {
     symbol: 'ORCL',
     companyName: 'Oracle Corporation',
     shares: 12,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Oracle_logo.svg/500px-Oracle_logo.svg.png'
   },
   {
     symbol: 'MSFT',
     companyName: 'Microsoft Corporation',
     shares: 32,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/500px-Microsoft_logo.svg.png'
   },
   {
     symbol: 'GOOGL',
     companyName: 'Alphabet Inc. (Google)',
     shares: 15,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/500px-Google_2015_logo.svg.png'
   },
   {
     symbol: 'AMZN',
     companyName: 'Amazon.com, Inc.',
     shares: 6,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/500px-Amazon_logo.svg.png'
   },
   {
     symbol: 'TSLA',
     companyName: 'Tesla, Inc.',
     shares: 12,
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Tesla_Motors.svg/500px-Tesla_Motors.svg.png'
   }
 ];
