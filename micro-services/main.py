20aa49a369f0819375bcc158f578df6ec7257b78
JENNIFER OJI <G00413791@atu.ie>
2026-01-20 16:14:45 +0000

    Set up FastAPI microservice

---
+++ micro-services/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/hello")
+def root():
+    return {"message": "This is the message"}
