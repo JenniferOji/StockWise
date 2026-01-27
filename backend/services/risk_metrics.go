3c3146f838b83547472bb9181f16e328ac276ae2
JENNIFER OJI <G00413791@atu.ie>
2026-01-27 22:01:54 +0000

    Implement Go backend function to call FastAPI risk metrics service (KAN-90)

--- backend/services/risk_metrics.go
+++ backend/services/risk_metrics.go
@@ -1,12 +1,12 @@
 package services
 
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"fmt"
-// 	"io"
-// 	"net/http"
-// )
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
 
 type Stock struct {
 	Ticker        string  `json:"ticker"`
@@ -24,3 +24,46 @@ type RiskMetricsResponse struct {
 	PortfolioValue float64           `json:"portfolio_value"`
 }
 
+func CalculateRiskMetrics(stocks []Stock) (*RiskMetricsResponse, error) {
+	requestBody := RiskMetricsRequest{
+		Stocks: stocks,
+	}
+
+	jsonData, err := json.Marshal(requestBody)
+	if err != nil {
+		return nil, err
+	}
+
+	// calling the fastAPI microservice
+	resp, err := http.Post(
+		//"http://localhost:8000/api/risk-metrics",
+		"http://127.0.0.1:8000/api/risk-metrics",
+		"application/json",
+		// bytes because http.Post requires an io.Reader
+		bytes.NewBuffer(jsonData),
+	)
+	if err != nil {
+		return nil, err
+	}
+	// ensures the response body is closed after reading
+	defer resp.Body.Close()
+
+	// read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// checking for non-200 status codes to know if there was an error
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("risk metrics service error: %s", string(body))
+	}
+
+	// parse the JSON response into RiskMetricsResponse struct
+	var result RiskMetricsResponse
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
