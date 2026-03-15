6085eebd89a6290d6370f24119afda6d7d2d445f
JenniferOji <127999049+JenniferOji@users.noreply.github.com>
2026-03-15 23:20:36 +0000

    Implement FastAPI performance metrics endpoint (KAN-88)

--- micro-services/performance_metrics_api.py
+++ micro-services/performance_metrics_api.py
@@ -46,3 +46,58 @@ def calculate_performance_metrics(portfolio_request: PortfolioRequest):
 		raise HTTPException(status_code=404, detail="No data for the tickers")
 
 	price_data = price_data.ffill().bfill().dropna(how="all")
+
+    # Calculate value for each stock in the portfolio
+	portfolio_value = {}
+	total_value = 0
+	returns = {}
+	
+    for ticker, holding in portfolio.items():
+		if ticker not in price_data:
+			continue
+
+		shares = holding['shares']
+		purchase_price = holding['purchase_price']
+		initial_price = price_data[ticker].iloc[0]
+		final_price = price_data[ticker].iloc[-1]
+		value = final_price * shares
+		total_value += value
+
+		# Total return for the period
+		returns[ticker] = (final_price - purchase_price) / purchase_price
+		portfolio_value[ticker] = value
+
+	if not returns:
+		raise HTTPException(status_code=404, detail="No valid tickers in data")
+
+	# calculating best and worst performer
+	best_performer = max(returns, key=returns.get)
+	worst_performer = min(returns, key=returns.get)
+
+	# calculating the overall portfolio return 
+	total_invested = sum(holding['shares'] * holding['purchase_price'] for holding in portfolio.values())
+	overall_return = (total_value - total_invested) / total_invested if total_invested else 0
+
+	# annualised return using thw CAGR formula: https://www.investopedia.com/terms/c/cagr.asp
+	n_years = portfolio_request.days / 365
+
+    if total_invested and n_years > 0:
+        growth_factor = total_value / total_invested
+        annual_growth_rate = 1 / n_years
+        cagr = (growth_factor ** annual_growth_rate) - 1
+    else:
+        cagr = 0
+
+	metrics = {
+		"overall_return": f"{overall_return * 100:.2f}%",
+		"annualized_return": f"{cagr * 100:.2f}%",
+		"returns_by_ticker": {k: f"{v * 100:.2f}%" for k, v in returns.items()}
+	}
+
+	return PerformanceMetricsResponse(
+		success=True,
+		metrics=metrics,
+		portfolio_value=total_value,
+		best_performer=best_performer,
+		worst_performer=worst_performer
+	)
