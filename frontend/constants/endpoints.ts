ef3c6aa17a3718bf63bc5b389618d66a44373f40
Jennifer Oji <g00413791@atu.ie>
2026-04-05 15:34:30 +0100

    Refactor server URL configuration to use environment variable

--- frontend/constants/endpoints.ts
+++ frontend/constants/endpoints.ts
@@ -1,9 +1,9 @@
 import { Platform } from 'react-native';
 import { get } from 'react-native/Libraries/NativeComponent/NativeComponentRegistry';
 
-const serverUrl = Platform.OS === 'web' ? 'http://localhost:8080/api' : 'http://172.18.0.3:8080'; // a device accessible IP for mobile
+// const serverUrl = Platform.OS === 'web' ? 'http://localhost:8080/api' : 'http://172.18.0.3:8080'; // a device accessible IP for mobile
 // const serverUrl = Platform.OS === 'web' ? 'https://ominous-doodle-pj7qx4vgg4qh944q-4000.app.github.dev/api' : 'https://ominous-doodle-pj7qx4vgg4qh944q-4000.app.github.dev/api'; // a device accessible URL for Codespaces
-// const serverUrl = process.env.EXPO_PUBLIC_API_URL;
+const serverUrl = process.env.EXPO_PUBLIC_API_URL;
 
 const user = "/user";
 const services = "/services";
